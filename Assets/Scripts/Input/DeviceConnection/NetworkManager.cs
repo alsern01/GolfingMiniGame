05d4b06ab08556307a1991e4bcf8e9a511c06bff
alsern01 <alsern01@ucm.es>
2023-12-31 13:17:48 +0100

    Iniciado el servidor local

--- Assets/Scripts/Input/DeviceConnection/NetworkManager.cs
+++ Assets/Scripts/Input/DeviceConnection/NetworkManager.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
+using Riptide;
+using Riptide.Utils;
 using UnityEngine;
 
 public class NetworkManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static NetworkManager instance;
+    public static NetworkManager Instance
     {
-        
+
+        get { return instance; }
+        private set
+        {
+            if (instance == null)
+            {
+                instance = value;
+            }
+            else if (instance != value)
+            {
+                Destroy(value);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public Server Server { get; private set; }
+
+    [SerializeField] private ushort port;
+    [SerializeField] private ushort maxClientCount;
+
+    private void Awake()
     {
-        
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        RiptideLogger.Initialize(Debug.Log, Debug.Log, Debug.LogWarning, Debug.LogError, false);
+
+        Server = new Server();
+        Server.Start(port, maxClientCount);
+    }
+
+    private void FixedUpdate()
+    {
+        Server.Update();
+    }
+
+    private void OnApplicationQuit()
+    {
+        Server.Stop();
     }
 }
